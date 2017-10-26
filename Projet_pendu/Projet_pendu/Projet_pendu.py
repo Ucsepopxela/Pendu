7b9fe97ee76e2f6d0df9a31c1290318ec5404bb5
Alex Popescu <alexvpopescu@live.com>
2017-10-26 18:30:14 +0200

    finally, the main program

--- Projet_pendu/Projet_pendu/Projet_pendu.py
+++ Projet_pendu/Projet_pendu/Projet_pendu.py
@@ -23,43 +23,120 @@ os.system('cls')
 while word1 != word2 and fautes <=6:
     h = 0
     letter = input("Enter a letter ")
-    for u in range(0, len(guessed)):
-        if guessed[u] == letter:
-            copy = 1
+    if len(letter) == 1 :   #checking that there is only one entered letter
+        for u in range(0, len(guessed)):
+            if guessed[u] == letter:
+                copy = 1    #checking if the letter has already been entered once
             
 
     
-    if copy == 0:
-        for c in range(0, len(word1)):
-            if letter == word1[c]:
-                word2[c] = letter
-                h +=1
+        if copy == 0:
+            for c in range(0, len(word1)):
+                if letter == word1[c]:
+                    word2[c] = letter
+                    h +=1    #replacing the "_" in word 2 by the actual letters
 
         
-        if h == 0:
-            fautes +=1
-        guessed.append(letter)
+            if h == 0:
+                fautes +=1   #mistakes counter
+
+            guessed.append(letter)
 
 
-    os.system('cls')
+        os.system('cls')
 
 
 
 
 
-    
-    print("You have the right to make ", 6-fautes, " more mistakes.")
-    print("Letters you have guessed : ", " ".join(guessed))
-    print("Number of mistakes : ", fautes,)
-    tmp = " ".join(word2)
-    print(tmp)
+        # Printing the results of each loop
+        print("You have the right to make ", 6-fautes, " more mistakes.")
+        print("Letters you have guessed : ", " ".join(guessed))
+        print("Number of mistakes : ", fautes,)
+        tmp = " ".join(word2)
+        print("Your word is :", tmp)
 
-    if copy !=0:
-        print("You've already guessed that letter !")
-    copy = 0
+        if copy !=0:
+            print("You've already guessed that letter !")
+        copy = 0
+        if fautes == 7:
+            print("  ------")
+            print("  |    |")
+            print(" ( )   |")
+            print(" /|\   |")
+            print("/ | \  |")
+            print(" / \   |")
+            print("/   \  |")
+            print("    ___|___")
 
+        if fautes == 6:
+            print("  ------")
+            print("  |    |")
+            print(" ( )   |")
+            print(" /|\   |")
+            print("/ | \  |")
+            print(" /     |")
+            print("/      |")
+            print("    ___|___")
+
+        if fautes == 5:
+            print("  ------")
+            print("  |    |")
+            print(" ( )   |")
+            print(" /|\   |")
+            print("/ | \  |")
+            print("       |")
+            print("       |")
+            print("    ___|___")
+
+        if fautes == 4:
+            print("  ------")
+            print("  |    |")
+            print(" ( )   |")
+            print(" /|    |")
+            print("/ |    |")
+            print("       |")
+            print("       |")
+            print("    ___|___")
+
+        if fautes == 3:
+            print("  ------")
+            print("  |    |")
+            print(" ( )   |")
+            print("  |    |")
+            print("  |    |")
+            print("       |")
+            print("       |")
+            print("    ___|___")
+
+        if fautes == 2:
+            print("  ------")
+            print("  |    |")
+            print(" ( )   |")
+            print("       |")
+            print("       |")
+            print("       |")
+            print("       |")
+            print("    ___|___")
+
+        if fautes == 1:
+            print("  ------")
+            print("  |    |")
+            print("       |")
+            print("       |")
+            print("       |")
+            print("       |")
+            print("       |")
+            print("    ___|___")
+    else:
+        print("You must write only ONE letter")
+
+        # printing the final results
 
 if fautes == 7:
-    print("You lost....")
+    print("You lost.... The word was :", word)
 elif fautes < 6 :
     print("Well done. You found '",word, "'")
+
+
+
