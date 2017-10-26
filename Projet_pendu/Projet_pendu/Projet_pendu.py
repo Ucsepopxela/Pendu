5c1ce28de9e707c1505185b2857fc811395502a0
Alex Popescu <alexvpopescu@live.com>
2017-10-26 17:03:23 +0200

    test lettres bonnes

--- Projet_pendu/Projet_pendu/Projet_pendu.py
+++ Projet_pendu/Projet_pendu/Projet_pendu.py
@@ -1,5 +1,27 @@
+import os
+
+
 word = "hello"
+word1=[]
 
 word = input("Enter the secret word ")
+word2 = ['a']
+
+for i in range(0, len(word)):
+    word1.append(word[i])
+
+while len(word) > len(word2):
+    word2.append("a")
+
+print(word1)
+print(word2)
+#os.system('cls')
 
 
+while word1 != word2 :
+    letter = input("Enter a letter ")
+    for c in range(0, len(word1)):
+        if letter == word1[c]:
+            word2[c] = letter
+        print(word1)
+        print(word2)
